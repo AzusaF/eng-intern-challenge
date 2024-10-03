ee928216243a5f91873aa2149238c17a792d0602
Azusa Fukuda <afukuda@mySeneca.ca>
2024-10-03 12:18:43 -0400

    implement translator

--- javascript/translator.js
+++ javascript/translator.js
@@ -32,6 +32,104 @@ const DECIMAL_FOLLOWS = '.O...O';
 const NUMBER_FOLLOWS = '.O.OOO';
 const SPACE = '......'
 
+/*
+Function to check if the string is English
+str: a string to be checked
+returns true or false
+*/
+const isEnglish=(str)=>{
+   for (let i = 0; i < str.length; i++){
+      if(str[i] != '.' && str[i] != 'O'){
+         return true;
+      }
+   }
+   return false;
+}
+
+/*
+Function to convert all the characters of a string (English) to Braille simbol
+str: a string to be converted
+returns a string converted to Braille simbol
+*/
+const englishToBraille = (str) =>{
+   let chars = [];
+   let res = '';
+   let isNum = false;
+   for (let i = 0; i < str.length; i++){
+      chars.push(str.substring(i,i+1));
+   }
+   for (let i = 0; i < str.length; i++){
+      if(isAlpha(chars[i])){
+         if(isCapital(chars[i])){
+            res += CAPTAL_FOLLOWS;
+            chars[i] = chars[i].toLowerCase()
+         }
+      } else if (isNumber(chars[i]) && !isNum){
+         res += NUMBER_FOLLOWS;
+         isNum = true;
+      } else if (isNum){
+         if(chars[i] == '.'){
+            res += DECIMAL_FOLLOWS;
+         }
+         if(chars[i] == ' '){
+            isNum = false;
+         }
+      }
+      if (chars[i] == ' '){
+         res += SPACE;
+      } else if (chars[i] == '.'){
+         // Do nothing as it is already dealt
+      } else {
+         res += getKeyByValue(BRAILLE_TO_ALPHANUMERIC, chars[i]);
+      }
+   }
+   return res;
+}
+
+/*
+Function to check if the charactor is alphabet
+ch: a charactor to be checked
+returns true or false
+*/
+const isAlpha = (ch)=>{
+   return typeof ch === "string" && ch.length === 1
+          && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
+}
+
+/*
+Function to check if the charactor is capitalized
+ch: a charactor to be checked
+returns true or false
+*/
+const isCapital= (ch)=>{
+   return (ch >= "A" && ch <= "Z")
+}
+
+/*
+Function to check if the charactor is number
+ch: a charactor to be checked
+returns true or false
+*/
+const isNumber= (ch)=>{
+   return (ch >= "0" && ch <= "9")
+}
+
+/*
+Function to get the key by value
+object: an object to be searched from
+value: a value to search
+returns the key
+*/
+const getKeyByValue=(object, value) =>{
+   return Object.keys(object).find(key =>
+      object[key][0] == value || BRAILLE_TO_ALPHANUMERIC[key][1] == value );
+}
+
+/*
+Function to convert all the characters of a string (Braille) to English
+str: a string to be converted
+returns a string converted to English
+*/
 const brailleToEnglish = (str) =>{
    let len = str.length;
    let numOfChars = len/6;
@@ -56,6 +154,7 @@ const brailleToEnglish = (str) =>{
                   res += '.'
                } else if(chars[i] == SPACE) {
                   isNum = false;
+                  res += ' ';
                } else {
                   temp = convertToEnglish(chars[i])
                   res += temp[1];
@@ -75,19 +174,34 @@ const brailleToEnglish = (str) =>{
    return res;
 }
 
+/*
+Function to convert charactor (Braille) to English letter
+ch: a string to be converted (represents a charactor)
+returns a charactor converted to English letter
+*/
 const convertToEnglish = (ch) =>{
    return BRAILLE_TO_ALPHANUMERIC[ch];
 }
 
+/*
+Function to determine English or Braille to convert
+input: a string to be converted 
+returns a string converted to either English or Braille
+*/
 const translate = (input) =>{
-   return brailleToEnglish(input)
+   if(isEnglish(input)){
+      return englishToBraille(input)
+   } else {
+      return brailleToEnglish(input)
+   }
 }
 
-const input = process.argv.slice(2).join(' ');
-if (!input) process.exit(1);
+// main
+const INPUT = process.argv.slice(2).join(' ');
+if (!INPUT) process.exit(1);
 
 try{
-   console.log(translate(input));
+   console.log(translate(INPUT));
 } catch (error){
    console.log("failed")
    console.log(error.message);
